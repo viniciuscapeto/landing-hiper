8e3f09fe9cd4463fcd66befa06f0700c085900ba
Vinicius Capeto <vi_capeto@yahoo.com.br>
2021-08-08 16:21:12 -0300

    Landing criada para entrevista de emprego, testando no git

---
+++ imagens/envia-dados.php
@@ -0,0 +1,4 @@
+<?php
+    echo "<script>alert('Formulário Foi enviado pelo método POST')</script>";
+    echo "<script>window.location = '".$site_url."';</script>";
+?>
---
+++ script.js
@@ -0,0 +1,40 @@
+function msgBotao(){
+    alert('Seus dados foram enviados!')
+    window.location.reload();
+}
+        
+$("#tel").mask("(99) 99999-9999");
+
+    
+function checkInputs(inputs) {
+var filled = true;
+
+inputs.forEach(function(input) {
+    
+    if(input.value === "") {
+        filled = false;
+    }
+
+});
+
+return filled;
+
+}
+var inputs = document.querySelectorAll("input");
+var button = document.querySelector("button");
+inputs.forEach(function(input) {
+    
+input.addEventListener("keyup", function() {
+    if(checkInputs(inputs)) {
+    button.disabled = false;
+    submitInicio.style.backgroundColor = "#FFC200";
+    submitInicio.style.color = "#6A4B9D";
+    submitInicio.style.border = "2px solid #6A4B9D";
+    submitInicio.style.boxShadow = "10px 10px 10px lightgrey";        
+    } else {
+    button.disabled = true;
+    submitInicio.style.backgroundColor = "#BEBEBE";
+    submitInicio.style.color = "#fff";
+    }
+});
+});
